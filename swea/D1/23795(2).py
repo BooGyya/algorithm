7679a599934f2dfef100780ab2526f48cac594ac
BooGyya <qhrud5263@gmail.com>
2026-03-01 15:11:42 +0900

    23795 - 우주괴물

--- swea/D1/23795(2).py
+++ swea/D1/23795(2).py
@@ -6,9 +6,10 @@ for tc in range(1, T+1):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
 
-    xr = xc = 0 # 괴물 좌표
-    count = 0
+    xr = xc = 0 # (xr, xc) 괴물 좌표
+    count = 0   # 안전한 빈 칸 
 
+    # 1. 괴물의 위치 찾기
     for r in range(N):
         for c in range(N):
             if arr[r][c] == 2:
@@ -19,7 +20,7 @@ for tc in range(1, T+1):
     dr = [-1, 1, 0, 0]
     dc = [0, 0, -1, 1]
     
-
+    # 2. 괴물마다 광선 발사
     for d in range(4):
         nr = xr + dr[d]
         nc = xc + dc[d]
@@ -36,7 +37,7 @@ for tc in range(1, T+1):
         # 이동한 칸이 범위를 넘거나 1(벽)을 만나면 while문 종료,
         # 방향(d)을 바꿔 다시 탐색 진행
 
-    
+    # 3. 안전한 빈 칸 세기
     for r in range(N):
         for c in range(N):
             if arr[r][c] == 0:
