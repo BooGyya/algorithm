25eff273b89f31fbe9f11d7b6dd47ae163293a99
hsy <haruzora1@naver.com>
2026-03-03 15:52:21 +0900

    5644  - 무선충전

--- swea/모의 sw 역량테스트/5644.py
+++ swea/모의 sw 역량테스트/5644.py
@@ -1,2 +1,12 @@
 # 5644 - 무선충전
 
+T = int(input())
+for tc in range(1, T+1):
+    M, A = map(int, input().split())
+    A_move = list(map(int, input().split()))
+    B_move = list(map(int, input().split()))
+    AP1 = list(map(int, input().split()))
+    AP2 = list(map(int, input().split()))
+    AP3 =list(map(int, input().split()))
+
+    print(f'#{tc} {}')
