f59aa6a35977b35d1dbdcba31890fcbb5df7ab8e
BooGyya <qhrud5263@gmail.com>
2026-03-11 23:58:35 +0900

    14510 - 나무 높이

--- swea/D2/14510.py
+++ swea/D2/14510.py
@@ -3,31 +3,38 @@
 '''
 홀수 날 +1
 짝수 날 +2
-
 최소 날짜 수 구하기
-
-1. 가장 큰 나무의 키 구하기
-
 '''
 
 T = int(input())
+
 for tc in range(1, T+1):
-
+    # 나무 개수
     N = int(input())
-    arr = list(map(int, input().split()))
-    result = 0
-    length_gap = []
-
-    max_length = max(arr)   # 가장 키 큰 나무 
-
-    # 가장 키가 큰 나무와 나머지 나무들의 갭 구하기
-    for i in arr:
-        length_gap.append(max_length-i)
+    # 각 나무의 높이
+    heights = list(map(int, input().split()))
+    # 가장 큰 나무의 높이
+    max_height = max(heights)
+    # 가장 적게 쓴 날짜
+    answer = 0
     
+    one_count = 0
+    two_count = 0
+    for height in heights:
+        if height == max_height:
+            continue
+        one_count += (max_height-height) % 2 # 0 또는 1
+        two_count += (max_height-height) // 2 # 최대한 2 블럭 확보
     
-
+    # 최대한 교차할 수 있을 때까지 > 2블럭 수와 1블럭 수의 차이가 가장 작을 때까지
+    while two_count - one_count > 1:
+        two_count -= 1
+        one_count += 2
     
-
-
-    print(f'#{tc} {result}')
+    if one_count > two_count:
+        answer = 2*one_count-1
+    else:
+        answer = 2*two_count
+    
+    print(f'#{tc} {answer}')
 
